3ba5bdb79d9249e0bf75dde14a275b104e1ac658
Adriano Moura <adrianodemoura@gmail.com>
2025-05-17 23:13:06 -0300

    correção no docker

--- docker/php/entrypoint.sh
+++ docker/php/entrypoint.sh
@@ -1,20 +1,8 @@
 #!/bin/sh
 set -e
 
-# Obter o UID e GID do diretório de volume montado
-HOST_UID=$(stat -c '%u' /var/www/html)
-HOST_GID=$(stat -c '%g' /var/www/html)
+# Aguardar alguns segundos para garantir que o volume foi montado
+sleep 2
 
-# Criar ou modificar grupo para corresponder ao GID do host
-if getent group $HOST_GID > /dev/null; then
-    GROUP_NAME=$(getent group $HOST_GID | cut -d: -f1)
-else
-    GROUP_NAME=hostgroup
-    addgroup -g $HOST_GID $GROUP_NAME
-fi
-
-# Garantir que www-data pertence ao grupo do host para permissões adequadas
-adduser www-data $GROUP_NAME
-
-# Executar o comando fornecido (normalmente php-fpm)
+# Executar o PHP-FPM
 exec "$@"
