28cd15d04c711bf1e9033309438d46ec46d8dcf2
Adriano Moura <adrianodemoura@gmail.com>
2025-05-24 16:31:02 -0300

    correção no build

--- src/scripts/build.sh
+++ src/scripts/build.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
+
+if [ ! -f docker-compose.yml ]; then 
+    cp .infra/docker/docker-compose.yml . ;
+    sed -i "s/user: \"1000:1000\"/user: \"$(id -u):$(id -u)\"/g" docker-compose.yml ;
+fi
+
+if [ ! -f src/.env ]; then cp .infra/env/.env src/.env ; fi
+
+if [ ! -f src/.env.local ]; then cp .infra/env/.env.local src/.env.local ; fi
+
 docker-compose down -v
 docker-compose build --no-cache
 docker-compose up -d --force-recreate
--- src/scripts/publicaProducao.sh
+++ src/scripts/publicaProducao.sh
@@ -1,17 +1,3 @@
 #!/bin/bash
 
-ssh -p 2225 ubuntu@cm.deskfacil.com '
-    cd ~/CloudMoura ;
-    git pull ;
-
-    if [ ! -f docker-compose.yml ]; then 
-        cp .infra/docker/docker-compose.yml . ;
-        sed -i "s/user: \"1000:1000\"/user: \"$(id -u):$(id -u)\"/g" docker-compose.yml ;
-    fi
-
-    if [ ! -f src/.env ]; then cp .infra/env/.env src/.env ; fi
-
-    if [ ! -f src/.env.local ]; then cp .infra/env/.env.local src/.env.local ; fi
-
-    src/scripts/build.sh
-'
+ssh -p 2225 ubuntu@cm.deskfacil.com 'cd ~/CloudMoura ; git pull ; src/scripts/build.sh'
