73ac6072f4566c14b79ccbcb57e7ac6cf4201edc
Adriano Moura <adrianodemoura@gmail.com>
2025-05-24 12:13:37 -0300

    correção ao pegar senha de email

--- src/Config/Mail.php
+++ src/Config/Mail.php
@@ -11,12 +11,13 @@ class Mail {
     public static function getConfig(): array {
 
         return [
-            'host'       => $_ENV['MAIL_HOST'],
-            'username'   => $_ENV['MAIL_USER'],
-            'password'   => $_ENV['MAIL_PASSWORD_APP'],
-            'port'       => $_ENV['MAIL_PORT'],
-            'smtp_secure'=> $_ENV['MAIL_SECURE'],
-            'from'       => $_ENV['MAIL_FROM'],
+            'host'          => $_ENV['MAIL_HOST'],
+            'username'      => $_ENV['MAIL_USER'],
+            'password'      => $_ENV['MAIL_PASSWORD'],
+            'password_app'  => $_ENV['MAIL_PASSWORD_APP'],
+            'port'          => $_ENV['MAIL_PORT'],
+            'smtp_secure'   => $_ENV['MAIL_SECURE'],
+            'from'          => $_ENV['MAIL_FROM'],
         ];
     }
 }
