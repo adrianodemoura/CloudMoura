fd154fe5033984f4446cdf73565b5a142339353f
Adriano Moura <adrianodemoura@gmail.com>
2025-05-24 11:30:43 -0300

    correção no plugin de publicação em produção

--- src/scripts/publicaProducao.sh
+++ src/scripts/publicaProducao.sh
@@ -3,6 +3,11 @@
 ssh -p 2225 ubuntu@cm.deskfacil.com "
     cd ~/CloudMoura ; 
     git pull ;
-    cp .infra/docker/docker-compose.yml docker-compose.yml ;
+
+    if [ ! -f docker-compose.yml ]; then cp .infra/docker/docker-compose.yml . ; fi
+
+    if [ ! -f .env ]; then cp .infra/docker/.env . ; fi
+    if [ ! -f .env.local ]; then cp .infra/docker/.env.local . ; fi
+    
     src/scripts/build.sh
     "
