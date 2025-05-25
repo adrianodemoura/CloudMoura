26bac0f9597ef0c121e8a1821c99c9a4e4343447
Adriano Moura <adrianodemoura@gmail.com>
2025-05-25 15:34:31 -0300

    correção no env.local e no build

--- src/scripts/build.sh
+++ src/scripts/build.sh
@@ -13,4 +13,4 @@ docker-compose down -v
 docker-compose build --no-cache
 docker-compose up -d --force-recreate
 docker-compose run --rm composer install
-docker-compose run --rm composer dump-autoload
+docker-compose run --rm composer dump-autoload --no-dev --optimize-autoloader
