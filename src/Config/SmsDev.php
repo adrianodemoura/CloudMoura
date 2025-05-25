e7475478dc81b770d090e2c006494b480d86d3ca
Adriano Moura <adrianodemoura@gmail.com>
2025-05-25 15:27:26 -0300

    correção ao configurar SmsDev

--- src/Config/SmsDev.php
+++ src/Config/SmsDev.php
@@ -6,7 +6,7 @@ class SmsDev {
     public static function getConfig(): array
     {
         return [
-            'smsdev_key' => isset( $_ENV['SMSDEV_API_KEY'] ) ? $_ENV['SMSDEV_API_KEY'] : null
+            'smsdev_key' => isset( $_ENV['SMSDEV_API_KEY'] ) ? $_ENV['SMSDEV_API_KEY'] : ''
         ];
     }
 }
