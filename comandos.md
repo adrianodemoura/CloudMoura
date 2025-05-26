6a7178809f68e484b5c6efeb655fc3c7113da511
Adriano Moura <adrianodemoura@gmail.com>
2025-05-26 15:36:09 -0300

    correção no script build.sh

---
+++ comandos.md
@@ -0,0 +1,6 @@
+# Comandos
+
+docker exec -it cloudmoura-php sh
+
+docker-compose run --rm composer install
+docker-compose run --rm composer dump-autoload --no-dev
--- src/scripts/build.sh
+++ src/scripts/build.sh
@@ -5,12 +5,20 @@ if [ ! -f docker-compose.yml ]; then
     sed -i "s/user: \"1000:1000\"/user: \"$(id -u):$(id -u)\"/g" docker-compose.yml ;
 fi
 
-if [ ! -f src/.env ]; then cp .infra/env/.env src/.env ; fi
+if [ ! -f src/.env ]; then 
+    cp .infra/env/.env src/.env ;
+fi
 
-if [ ! -f src/.env.local ]; then cp .infra/env/.env.local src/.env.local ; fi
+if [ ! -f src/.env.local ]; then
+    cp .infra/env/.env.local src/.env.local ; 
+fi
 
 docker-compose down -v
 docker-compose build --no-cache
 docker-compose up -d --force-recreate
-docker-compose run --rm composer install
-docker-compose run --rm composer dump-autoload --no-dev
+
+// se não existir a pasta vendor, cria ela.
+if [ ! -d src/vendor ]; then
+    docker-compose run --rm composer install
+    docker-compose run --rm composer dump-autoload --no-dev
+fi
